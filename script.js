c731fc5926d8b934845e28df8abb3633cff1cfbc
Nathan-Simonnet <nathan.simonnet@gmail.com>
2023-10-31 09:43:24 +0100

    range value add in HTML (span)

---
+++ script.js
@@ -0,0 +1,7 @@
+const result = document.getElementById('result');
+const range = document.getElementById('range');
+const lowercase = document.getElementById('lowercase');
+const uppercase = document.getElementById('uppercase');
+const number = document.getElementById('number');
+const symbols = document.getElementById('symbols');
+const submited = document.getElementById('submited');
